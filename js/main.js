d75da37a4d86204177c69969ccbf303b60c4a1df
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-08-05 12:29:04 +0900

    check-fix

--- js/main.js
+++ js/main.js
@@ -20,6 +20,13 @@ $(function () {
         $("#js-hamburger").click(function () {
         $(this).toggleClass("active");
         $("#js-nav").toggleClass("active");
+
+        // ナビゲーション展開時にスクロール制御
+        if ($("#js-nav").hasClass("active")) {
+            $("body").css("overflow", "hidden");
+        } else {
+            $("body").css("overflow", "");
+        }
     });
 
     // メニュー項目のクリック時にメニューを閉じる
