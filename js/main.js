daf5a45b663929f2838320cbcdd05e060d50b393
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-10-01 12:11:31 +0900

    animation_add

--- js/main.js
+++ js/main.js
@@ -36,3 +36,40 @@ $(function () {
         $("body").css("overflow", "");
     });
 });
+
+
+// オープニング
+$(function() {
+	setTimeout(function(){
+		$('.js-opening').fadeIn(1600);
+	},400); //0.4秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
+
+
+
+// トリガー取得
+const scrollTrigger = document.querySelectorAll('.js-scroll-trigger');
+
+// aimation呼び出し
+if (scrollTrigger.length) {
+    scrollAnimation(scrollTrigger);
+}
+
+// aimation関数
+function scrollAnimation(trigger) {
+    window.addEventListener('scroll', function () {
+        for (var i = 0; i < trigger.length; i++) {
+            let position = trigger[i].getBoundingClientRect().top,
+                scroll = window.pageYOffset || document.documentElement.scrollTop,
+                offset = position + scroll,
+                windowHeight = window.innerHeight;
+
+            if (scroll > offset - windowHeight + 200) {
+                trigger[i].classList.add('is-active');
+            }
+        }
+    });
+}
